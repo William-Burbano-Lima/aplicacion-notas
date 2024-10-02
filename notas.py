4a1b3fae7f8e8451a2b790614f87571f17e5ec3d
William Burbano Lima <shamukproject@gmail.com>
2024-10-02 15:13:40 -0500

    Primer commit: crear archivo notas.py

---
+++ notas.py
@@ -0,0 +1,18 @@
+def agregar_nota(nota):
+    with open("notas.txt", "a") as archivo:
+        archivo.write(nota + "\n")
+    print("Nota agregada.")
+
+def mostrar_notas():
+    try:
+        with open("notas.txt", "r") as archivo:
+            notas = archivo.readlines()
+        print("Notas:")
+        for nota in notas:
+            print(nota.strip())
+    except FileNotFoundError:
+        print("No hay notas guardadas.")
+
+if __name__ == "__main__":
+    agregar_nota("Esta es una nota de prueba.")
+    mostrar_notas()
